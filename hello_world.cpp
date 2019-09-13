64fe0872121673f257896711cb36bcead1c31926
Ubuntu <ubuntu@ip-172-31-30-160.ec2.internal>
2019-09-13 01:17:12 +0000

    added hello world program

---
+++ hello_world.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2019 Marwan Mashaly
+//
+// Created by : Marwan Mashaly
+// created on : September 2019
+// This is the "Hello, World!" program 
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello, World" << std::endl;
+}
